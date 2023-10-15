1888c778959c73bc244c11c65aa248eeb6e56a7b
Juan Pablo <carabali.juan@correounivalle.edu.co>
2023-10-15 15:45:16 -0500

    Cambios para desplegar

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,28 @@
-import Bee from "./World/Bee";
-import Tree from "./World/Tree";
-import Info from "./Info/Info";
-import React from 'react';
-
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Experience = () => {
+    const boxRef = useRef();
+
+    useFrame((state, delta)=>{
+        boxRef.current.rotation.x += 1 * delta;
+    })
+
     return (
         <>
-            <Info name='Juan Pablo Carabali Quiroz' bio='systems engineering student'/>
-
-
-
+            <OrbitControls
+                makeDefault
+            />
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} intensity={2} />
+            <mesh ref={boxRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshBasicMaterial wireframe={false} position={[10, 10, 5]} color="purple" />
+            </mesh>
         </>
+
     )
 }
+
 export default Experience;
